d501031e48d0b6e598098c06705fabd9b4bd82fa
tosinchukwu <tosin.mouseng20@gmail.com>
2026-07-16 23:14:54 +0100

    Remove obsolete useContract file (wagmi v2 removal)

--- hooks/useContract.ts
+++
@@ -1,7 +0,0 @@
-import { useContract } from 'wagmi';
-import { contractConfig } from '@/lib/contract';
-export function useContractInstance() {
-  return useContract({
-    ...contractConfig,
-  });
-}
