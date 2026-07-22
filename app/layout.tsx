ef8b0b7db46f648a9409c361556ab541d13c7937
Tosin Olaitan <78284223+tosinchukwu@users.noreply.github.com>
2026-07-22 17:02:53 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} theme-bg theme-text min-h-screen`}>
         <Providers>{children}</Providers>
       </body>
     </html>
